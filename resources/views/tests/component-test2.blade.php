6b1f73f46f4e68694009c97ac8ea0ef39c1dd815
yuu5m77@gmail.com <yuu5m77@gmail.com>
2024-09-19 20:23:07 +0900

    クラスベース

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,4 +2,7 @@
 <x-slot name="header">ヘッダー2</x-slot>
 
 コンポーネントテスト2
+<x-test-class-base classBaseMessage="メッセージです"/>
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値にから変更しています" />
 </x-tests.app>
